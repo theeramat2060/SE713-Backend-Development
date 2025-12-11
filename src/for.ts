8ddf3c15c1a131fcf424294d9c744c52ffad763c
theeramat2060 <theeramat_m@cmu.ac.th>
2025-12-11 22:44:15 +0700

    task10

--- src/for.ts
+++ src/for.ts
@@ -6,15 +6,25 @@
 // for (let i = 0; i < color.length; i++) {
 //     console.log(color[i])
 // }
+//
+// let names = ["Somchai", "Ann", "Waraporn", "Tom", "Suda"];
+//
+// for (let i = 0; i < names.length; i++) {
+//     let name = names[i];
+//
+//     if (name.length > 6) {
+//         console.log(`สวัสดีคุณ ${name}`);
+//     } else {
+//         console.log(`Hello ${name}`);
+//     }
+// }
 
-let names = ["Somchai", "Ann", "Waraporn", "Tom", "Suda"];
+// let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+// for (const c of color) {
+//     console.log(c);
+// }
 
-for (let i = 0; i < names.length; i++) {
-    let name = names[i];
-
-    if (name.length > 6) {
-        console.log(`สวัสดีคุณ ${name}`);
-    } else {
-        console.log(`Hello ${name}`);
-    }
+let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+for (const index in color) {
+    console.log(`Index: ${index}, Value: ${color[index]}`);
 }
