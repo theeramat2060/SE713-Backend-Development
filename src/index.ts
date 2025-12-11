5fffcc001325f70192dfa5d41b35bf132bf6ef52
theeramat2060 <theeramat_m@cmu.ac.th>
2025-12-11 22:38:00 +0700

    task8

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello, TypeScript with Node.js!");
+// let x:number = 11
+// let y:number = 11
+// let z:number = 0
+//
+// if (x > 5  &&  y > 12) {
+//     console.log('ค่ามากจัง')
+// }else {
+//     z =((x*y) +(x/y))
+//     console.log(z)
+// }
+
+//
+// let x =7
+// if (x > 10) {
+//     console.log('x is more than 10')
+// } else if (x < 5){
+//     console.log('x is less than 5')
+// }
+// else {
+//     console.log('x is between 5 and 10')
+// }
+
+let score = 75;
+
+if (score > 80) {
+    console.log("A");
+} else if (score > 60) {
+    console.log("B");
+} else if (score > 50) {
+    console.log("C");
+} else {
+    console.log("F");
+}
