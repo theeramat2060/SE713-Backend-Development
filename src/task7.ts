c2f7ffa05237a63e26e98cea5e1279fb9b3509cd
theeramat2060 <theeramat_m@cmu.ac.th>
2025-12-11 22:28:22 +0700

    task6
    task6

---
+++ src/task7.ts
@@ -0,0 +1,61 @@
+// let x = 10;
+// x = 15;
+// console.log(x);
+
+// const x = 10;
+// x = 15;
+// console.log(x);
+//
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+//
+// let a: Person = {
+//     name: 'Alice',
+//     age: 30
+// }
+//
+// a = {
+//     name: 'Bob',
+//     age: 25
+// }
+// console.log(a);
+//
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+//
+//
+// const a: Person = {
+//     name: 'Alice',
+//     age: 30
+// }
+//
+//
+// a = {
+//     name: 'Bob',
+//     age: 25
+// }
+// console.log(a)
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+
+const a: Person = {
+    name: 'Alice',
+    age: 30
+}
+
+
+a.name = 'Bob'
+a.age = 25
+console.log(a)
+
+const list = [1, 2, 3, 4, 5];
+console.log(list);
+
