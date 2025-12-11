e27d055b831a943a726f5dd84c1e2ff3521ab7cc
theeramat2060 <theeramat_m@cmu.ac.th>
2025-12-11 22:42:45 +0700

    task9

---
+++ src/for.ts
@@ -0,0 +1,20 @@
+// for (let i = 1; i < 10; i++) {
+//     console.log(i);
+// }
+
+// let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+// for (let i = 0; i < color.length; i++) {
+//     console.log(color[i])
+// }
+
+let names = ["Somchai", "Ann", "Waraporn", "Tom", "Suda"];
+
+for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+
+    if (name.length > 6) {
+        console.log(`สวัสดีคุณ ${name}`);
+    } else {
+        console.log(`Hello ${name}`);
+    }
+}
